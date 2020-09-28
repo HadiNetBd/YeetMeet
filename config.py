eb1a11662e7033fe72a458cd9012360478ae3b98
HadiNetBd <70646018+HadiNetBd@users.noreply.github.com>
2020-09-28 10:49:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
 	BOT_TOKEN = os.environ.get('BOT_TOKEN', '1302149312:AAEj3coBAZGsUU_a4EPGjxpmXW26dbqDj98')
-	USERNAME = os.environ.get('abchadi09@gmail.com')
-	PASSWORD = os.environ.get('hara1128')
+	USERNAME = os.environ.get('USERNAME, 'abchadi09@gmail.com')
+	PASSWORD = os.environ.get('PASSWORD', 'hara1128')
